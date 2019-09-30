c69bc3c5780a9317c6443a17bd70d5421c6e9e48
reyjroliva <reyjroliva@ufl.edu>
2019-09-30 10:50:25 -0400

    Added start screen/Login UI

--- Local/ContentView.swift
+++ Local/ContentView.swift
@@ -9,8 +9,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username = ""
+    @State private var password = ""
+    
     var body: some View {
-        Text("Hello World")
+        VStack(alignment: .leading) {
+            Text("Welcome to Local!")
+                .font(.largeTitle)
+                .multilineTextAlignment(.center)
+            Text("Username")
+                .font(.headline)
+                .padding(EdgeInsets(top: 50.0, leading: 10.0, bottom: 0.0, trailing: 0.0))
+            TextField("Username", text: $username)
+                .frame(width: 250.0, height: 30.0)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(EdgeInsets(top: 10.0, leading: 10.0, bottom: 0.0, trailing: 0.0))
+            Text("Password")
+                .font(.headline)
+                .padding(EdgeInsets(top: 20.0, leading: 10.0, bottom: 0.0, trailing: 0.0))
+            TextField("Password", text: $password)
+                .frame(width: 250.0, height: 30.0)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(EdgeInsets(top: 10.0, leading: 10.0, bottom: 0.0, trailing: 0.0))
+            Button(action: {}) {
+                Text("Login")
+                    .font(.title)
+            }
+            .padding(EdgeInsets(top: 50.0, leading: 10.0, bottom: 0.0, trailing: 0.0))
+            Text("Don't have an account?")
+                .font(.footnote)
+                .padding(EdgeInsets(top: 100.0, leading: 10.0, bottom: 0.0, trailing: 0.0))
+            Button(action: {}) {
+                Text("Sign Up")
+                    .font(.title)
+            }
+            .padding(EdgeInsets(top: 10.0, leading: 10.0, bottom: 0.0, trailing: 0.0))
+        }
     }
 }
 
