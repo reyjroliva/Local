7076f7979583b270faad55c3b7f7cf263b1c25f0
reyjroliva <reyjroliva@ufl.edu>
2019-10-02 14:33:48 -0400

    Began  creating sign up UI

--- Local/SignUpView.swift
+++ Local/SignUpView.swift
@@ -10,8 +10,63 @@ import Foundation
 import SwiftUI
 
 struct SignUpView: View {
+    
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    @State private var username = ""
+    @State private var password = ""
+    
+    var backBtn : some View {
+        Button(action: {
+            self.presentationMode.wrappedValue.dismiss()
+        }) {
+            HStack {
+                Image("ic_back")
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(.white)
+                Text("< Login")
+            }
+        }
+    }
+    
+    
     var body: some View {
-        Text("Sign Up Page")
+        NavigationView {
+            VStack(alignment: .center) {
+                Text("Create and Account with Local!")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.blue)
+                VStack(alignment: .leading) {
+                    Text("Username")
+                        .font(.headline)
+                        .fontWeight(.bold)
+                        .padding(.top, 10.0)
+                        .padding(.leading, 10.0)
+                        .foregroundColor(.white)
+                    TextField("Username", text: $username)
+                        .frame(width: 250.0, height: 30.0)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.leading, 10.0)
+                        .padding(.trailing, 10.0)
+                    Text("Password")
+                        .font(.headline)
+                        .fontWeight(.semibold)
+                        .padding(.top, 20.0)
+                        .padding(.leading, 10.0)
+                        .foregroundColor(.white)
+                    TextField("Password", text: $password)
+                        .frame(width: 250.0, height: 30.0)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.leading, 10.0)
+                        .padding(.trailing, 10.0)
+                        .padding(.bottom, 15.0)
+                }
+                .background(Color.blue)
+                .cornerRadius(15.0)
+            }
+        }
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading: backBtn)
     }
 }
 
