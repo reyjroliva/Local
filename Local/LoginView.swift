13ff8dd1b59c2956140d579afca549631d1dac1c
reyjroliva <reyjroliva@ufl.edu>
2019-10-12 17:43:13 -0400

    Format LoginView adding modularization

--- Local/LoginView.swift
+++ Local/LoginView.swift
@@ -9,48 +9,77 @@
 import SwiftUI
 
 struct LoginView: View {
-    @State private var username = ""
-    @State private var password = ""
+    
+    init() {
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor.blue]
+    }
     
     var body: some View {
         NavigationView {
-            VStack(alignment: .leading) {
-                Text("Username")
-                    .font(.headline)
-                    .padding(.top, 10.0)
-                TextField("Username", text: $username)
-                    .frame(width: 250.0, height: 30.0)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding(.top, 10.0)
-                Text("Password")
-                    .font(.headline)
-                    .padding(.top, 20.0)
-                TextField("Password", text: $password)
-                    .frame(width: 250.0, height: 30.0)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding(.top, 10.0)
+            VStack(alignment: .center, spacing: 10.0) {
+                loginInputFields()
                 NavigationLink(destination: HomeView()) {
                     Text("Login")
                         .font(.title)
-                        .foregroundColor(.blue)
+                        .foregroundColor(Color.white)
+                        .multilineTextAlignment(.center)
+                        .frame(width: 270.0, height: 50.0, alignment: .center)
                 }
-                .padding(.top, 25.0)
+                .background(Color.blue)
+                .cornerRadius(15.0)
+                .padding(.bottom, 140.0)
                 Text("Don't have an account?")
-                    .font(.footnote)
-                    .padding(.top, 100.0)
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
                 NavigationLink(destination: SignUpView()) {
                     Text("Sign Up")
                         .font(.title)
-                        .foregroundColor(.blue)
+                        .foregroundColor(Color.white)
+                        .multilineTextAlignment(.center)
+                        .frame(width: 270.0, height: 50.0, alignment: .center)
                 }
+                .background(Color.blue)
+                .cornerRadius(15.0)
             }
-        .navigationBarTitle(Text("Welcome To Local!"))
+            .navigationBarTitle(Text("Welcome To Local!"))
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-       LoginView()
+        LoginView()
+    }
+}
+
+struct loginInputFields: View {
+    @State private var username = ""
+    @State private var password = ""
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10.0) {
+            Text("Username")
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding(.top, 10.0)
+                .padding(.leading, 10.0)
+            TextField("Username", text: $username)
+                .frame(width: 250.0, height: 30.0)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.bottom, 10.0)
+                .padding(.horizontal, 10.0)
+            Text("Password")
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding(.top, 10.0)
+                .padding(.leading, 10.0)
+            TextField("Password", text: $password)
+                .frame(width: 250.0, height: 30.0)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.bottom, 15.0)
+                .padding(.horizontal, 10.0)
+        }
+        .background(Color.blue)
+        .cornerRadius(15.0)
     }
 }
