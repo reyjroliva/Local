eca65e0637f4d1e6aefd8c04db22be2f874330ab
reyjroliva <reyjroliva@ufl.edu>
2019-11-06 13:28:42 -0500

    Populated social media tab with dummy data

--- Local/SocialMediaViewTab/SocialMediaItemView.swift
+++ Local/SocialMediaViewTab/SocialMediaItemView.swift
@@ -9,20 +9,44 @@
 import Foundation
 import SwiftUI
 
+struct mediaPost: Identifiable {
+    var id = UUID()
+    var image: Image
+    enum postSource {
+        case ig, tw, fb
+    }
+    var source: postSource
+}
+
 struct socialMediaItemView: View {
+    var post: mediaPost
+    
     var body: some View {
-        ZStack(alignment: .bottomTrailing) {
-            Image(systemName: "circle.grid.3x3.fill")
+        return ZStack(alignment: .bottomTrailing) {
+            post.image
                 .font(.largeTitle)
-                .frame(minWidth: 0, idealWidth: 0, maxWidth: .infinity, minHeight: 0, idealHeight: 0, maxHeight: 300)
-            Image(systemName:"rectangle.grid.2x2")
-                .font(.largeTitle)
+                .frame(minWidth: 0, idealWidth: 300, maxWidth: 500, minHeight: 300, idealHeight: 300, maxHeight: 300)
+            if(post.source == .ig) {
+                Image(systemName: "camera.fill")
+                    .font(.title)
+                    .foregroundColor(Color(UIColor.systemPink))
+            } else if(post.source == .tw) {
+                Image(systemName: "paperplane.fill")
+                    .font(.title)
+                    .foregroundColor(Color(UIColor.systemBlue))
+            } else if (post.source == .fb){
+                Image(systemName: "faceid")
+                    .font(.title)
+                    .foregroundColor(Color(UIColor.blue))
+            }
         }
+        .background(Color(UIColor.systemGray))
     }
 }
 
 struct socialMediaItemView_Preview: PreviewProvider {
+    @State static var previewPost = mediaPost(image: Image(systemName: "circle.grid.2x2.fill"), source: mediaPost.postSource.fb)
     static var previews: some View {
-        socialMediaItemView()
+        socialMediaItemView(post: previewPost)
     }
 }
--- Local/SocialMediaViewTab/SocialMediaView.swift
+++ Local/SocialMediaViewTab/SocialMediaView.swift
@@ -10,8 +10,21 @@ import Foundation
 import SwiftUI
 
 struct socialMediaView: View {
+    @State var posts = [mediaPost(image: Image(systemName: "square.and.arrow.up"), source: .ig),
+                        mediaPost(image: Image(systemName: "trash"), source: .ig),
+                        mediaPost(image: Image(systemName: "cloud.snow.fill"), source: .fb),
+                        mediaPost(image: Image(systemName: "cloud.bolt"), source: .tw),
+                        mediaPost(image: Image(systemName: "flame.fill"), source: .fb),
+                        mediaPost(image: Image(systemName: "timelapse"), source: .tw),
+                        mediaPost(image: Image(systemName: "mic.fill"), source: .ig),
+                        mediaPost(image: Image(systemName: "suit.heart.fill"), source: .fb),
+                        mediaPost(image: Image(systemName: "bell"), source: .ig),
+                        mediaPost(image: Image(systemName: "bolt.fill"), source: .fb),
+                        mediaPost(image: Image(systemName: "flashlight.on.fill"), source: .tw)]
+    
     var body: some View {
-        VStack(alignment: .center) {
+        ScrollView {
+            Divider()
             Text("Welcome to Local's Feed!")
                 .font(.title)
                 .fontWeight(.semibold)
@@ -21,21 +34,31 @@ struct socialMediaView: View {
                 .fontWeight(.semibold)
                 .foregroundColor(Color(UIColor.systemBlue))
             HStack(alignment: .center, spacing: 5.0) {
+                Spacer()
                 Image(systemName: "camera.fill")
                     .font(.title)
                     .foregroundColor(Color(UIColor.systemPink))
                 Text("Local")
                     .font(.headline)
+                Spacer()
                 Image(systemName: "paperplane.fill")
                     .font(.title)
                     .foregroundColor(Color(UIColor.systemBlue))
                 Text("@Local")
                     .font(.headline)
+                Spacer()
                 Image(systemName: "faceid")
                     .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
                     .foregroundColor(Color(UIColor.blue))
                 Text("@Local")
                     .font(.headline)
+                Spacer()
+            }
+            Divider()
+            ForEach(posts) { post in
+                socialMediaItemView(post: post)
+                .padding()
+                Divider()
             }
         }
     }
